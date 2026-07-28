f0cb028e08c5b246a8fed29558fb008980be2a45
fangwl591021 <fangwl591021@gmail.com>
2026-07-29 00:29:13 +0800

    feat: add homepage product banner

--- test/brand-theme.test.js
+++ test/brand-theme.test.js
@@ -13,5 +13,5 @@ test("public site uses the logo-derived A-kaffit rust theme", () => {
   assert.match(css, /\.ak-bottom-nav\{[^}]*var\(--ak-primary\)[^}]*var\(--ak-deep\)/);
   assert.match(css, /\.ak-feature-grid button\{[^}]*color:var\(--ak-primary\)/);
   assert.doesNotMatch(css, /#003f2d|#002d21|#24e56f|#004932|#003b2c/);
-  assert.match(html, /\/akaffit-20260729-8\.css/);
+  assert.match(html, /\/akaffit-20260729-9\.css/);
 });
