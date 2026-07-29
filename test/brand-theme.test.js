f9c160a81b99c23f069c0b2d8ba29b346a72cf42
fangwl591021 <fangwl591021@gmail.com>
2026-07-29 22:50:11 +0800

    tint social navigation background

--- test/brand-theme.test.js
+++ test/brand-theme.test.js
@@ -21,6 +21,8 @@ test("public site uses the coffee-inspired A-kaffit rust theme", () => {
   assert.match(css, /\.ak-home-content>\.ak-content-tabs\{[^}]*safe-area-inset-bottom/);
   assert.match(css, /\.ak-home-content>\.ak-content-tabs\{[^}]*grid-template-columns:repeat\(5/);
   assert.doesNotMatch(css, /#003f2d|#002d21|#24e56f|#004932|#003b2c/);
-  assert.match(html, /\/akaffit-20260729-26\.css/);
+  assert.match(css, /\.ak-home-content>\.ak-content-tabs\{[^}]*background:var\(--ak-soft\)/);
+  assert.match(css, /\.ak-home-content>\.ak-content-tabs button\.active\{[^}]*background:#f3d2c1/);
+  assert.match(html, /\/akaffit-20260729-27\.css/);
   assert.match(html, /\/app-20260729-94\.js/);
 });
