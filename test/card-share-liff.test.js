b97e100638fe4861f3ea8e64c93c00b71c507c8a
fangwl591021 <fangwl591021@gmail.com>
2026-08-01 11:08:45 +0800

    fix: add LINE card share fallback

--- test/card-share-liff.test.js
+++ test/card-share-liff.test.js
@@ -14,4 +14,8 @@ test("personal cards use the dedicated LIFF share app without replacing member l
   assert.match(app, /function cardSharePickerUrl\(cardId\)[\s\S]*state\.config\?\.cardShareLiffId \|\| state\.config\?\.liffId/);
   assert.match(app, /url\.searchParams\.set\("shareCardId", cardId\)[\s\S]*url\.searchParams\.set\("share", "1"\)/);
   assert.match(app, /async function sharePersonalCard\(card\)[\s\S]*location\.assign\(cardSharePickerUrl\(card\.id\)\)/);
+  assert.match(app, /function cardLineShareUrl\(cardId, card = null\)[\s\S]*https:\/\/line\.me\/R\/share\?text=/);
+  assert.match(app, /!liff\.isApiAvailable\?\.\("shareTargetPicker"\)[\s\S]*lineShareFallbackUrl = cardLineShareUrl\(cardId, publicCardResult\.card\)/);
+  assert.match(app, /not allowed\|not available\|shareTargetPicker[\s\S]*lineShareFallbackUrl = cardLineShareUrl\(cardId, publicCardResult\?\.card\)/);
+  assert.match(app, /if \(lineShareFallbackUrl\)[\s\S]*location\.replace\(lineShareFallbackUrl\)[\s\S]*else if \(pickerFinished\)/);
 });
